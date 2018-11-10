47f83989e8d057e5b2021fe8845a5709e5207395
Antoine <antoine.sb@orange.fr>
2018-11-10 11:45:30 +0000

    read player names

--- main.rs
+++ main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
+fn get_keyboard_input() -> String {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("{} bytes read", n);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+    return input;
+}
+
 fn main() {
-    println!("Hello, world !");
+    let mut player1_name = String::new();
+    let mut player2_name = String::new();
+    {
+        println!("Enter player one name");
+        player1_name = get_keyboard_input();
+        println!("{}", player1_name);
+    }
+    {
+        println!("Enter player two name");
+        player2_name = get_keyboard_input();
+        println!("{}", player1_name);
+    }
+     
 }
