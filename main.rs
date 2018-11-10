8740f0e8489c3b0b3bf5bf0221209d77edcb83af
Antoine <antoine.sb@orange.fr>
2018-11-10 13:02:34 +0000

    add Player structs + main loop

--- main.rs
+++ main.rs
@@ -1,28 +1,47 @@
 use std::io;
 
+struct Player {
+    name: String,
+    score: u8,
+    games_won: u8,
+}
+
 fn get_keyboard_input() -> String {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            println!("{} bytes read", n);
-        }
+        Ok(n) => println!("{} bytes read", n),
         Err(error) => println!("error: {}", error),
     }
     return input;
 }
 
 fn main() {
-    let mut player1_name = String::new();
-    let mut player2_name = String::new();
+    let mut player1 = Player {
+        name: String::new(),
+        score: 0,
+        games_won: 0,
+    };
+    let mut player2 = Player {
+        name: String::new(),
+        score: 0,
+        games_won: 0,
+    };
     {
         println!("Enter player one name");
-        player1_name = get_keyboard_input();
-        println!("{}", player1_name);
+        player1.name = get_keyboard_input();
     }
     {
         println!("Enter player two name");
-        player2_name = get_keyboard_input();
-        println!("{}", player1_name);
+        player2.name = get_keyboard_input();
     }
-     
+    println!("PLayer one is {} and Player two is {}", player1.name, player2.name);
+    
+    let mut count = 0u8;
+    loop {
+        println!("{}", count);
+        count += 1;
+        if count == 5 {
+            break;
+        }
+    }
 }
