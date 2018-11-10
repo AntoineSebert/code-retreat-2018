313c386352d02483b14c24ed419b1036068b2650
Antoine <antoine.sb@orange.fr>
2018-11-10 17:30:30 +0000

    refactor + support command-line arguments

--- main.rs
+++ main.rs
@@ -1,57 +1,94 @@
 use std::io;
+use std::env;
+use std::process;
 
+/*
+ * Holds the player data.
+ */
 struct Player {
     name: String,
     score: u8,
     games_won: u8,
 }
 
+/*
+ * Get a trimmed String from the keyword.
+ */
 fn get_keyboard_input() -> String {
     let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(n) => println!("{} bytes read", n),
-        Err(error) => println!("error: {}", error),
-    }
-    return input;
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    return input.trim().to_string();
 }
 
-fn get_player_number(player1_name: String, player2_name: String) -> u8 {
-    loop {
-        println!("Enter 1 for {}, or 2 for {}", player1_name, player2_name);
-        let input = get_keyboard_input();  
-        match input.parse::<u8>() {
-            Ok(n) => {
-                if n == 1 || n == 2 {
-                    return n;
-                }
-            },
-            Err(error) => println!("error: {}", error), 
-        }
+/*
+ * Get a u8 from the keyboard.
+ * Value can be 0.
+ */
+fn get_parsed_input() -> u8 {
+    let input = get_keyboard_input();  
+    match input.parse::<u8>() {
+        Ok(n) => return n,
+        Err(error) => {
+            println!("error: {}", error);
+            return 0;
+        }, 
     }
-} 
+}
+
+/*
+ * Get a valid player number, between 0 and PLAYER_NUMBER
+ */
+fn get_player_number(player_number: u8) -> u8 {
+    loop {
+        println!("Enter Player number, between 0 and {}", player_number);
+        let number = get_parsed_input(); 
+        if number < player_number {
+            return number;
+        }
+    } 
+}
+
+/*
+ * Returns an array of Player of size PLAYER_NUMBER, initialized with a user-defined name.
+ * Does not check for the reuse of names nor empty names.
+ */
+fn initialize_players(player_number: u8) -> Vec<Player> {
+    let mut players = Vec::new();
+    let mut n = 0;
+    while n < player_number {
+        println!("Enter player name");
+        players.push(Player {
+            name: get_keyboard_input(),
+            score: 0,
+            games_won: 0,
+        });
+        println!("Player name is {}", players[players.len() - 1].name);
+        n += 1;
+    }
+    return players;
+}
 
 fn main() {
-    let mut player1 = Player {
-        name: String::new(),
-        score: 0,
-        games_won: 0,
-    };
-    let mut player2 = Player {
-        name: String::new(),
-        score: 0,
-        games_won: 0,
-    };
-
-    {
-        println!("Enter player one name");
-        player1.name = get_keyboard_input();
-    }
-    {
-        println!("Enter player two name");
-        player2.name = get_keyboard_input();
-    }
-    println!("PLayer one is {} and Player two is {}", player1.name, player2.name);
+    let args: Vec<String> = env::args().collect();
     
+    // check arguments number
+    if args.len() != 2 {
+        println!("Incorrect number of arguments - correct syntax : main.exe number");
+        process::exit(1);
+    }
+
+    // check the candidate argument for the player number
+    let player_number: u8;
+    match &args[1].parse::<u8>() {
+        &Ok(n) => player_number = n,
+        &Err(ref error) => {
+            println!("error: {}", error);
+            process::exit(1);
+        },
+    }
+ 
+    let players: Vec<Player> = initialize_players(player_number);
+ 
     let mut count = 0u8;
     loop {
         println!("{}", count);
@@ -59,5 +96,5 @@ fn main() {
         if count == 5 {
             break;
         }
-    }
+    } 
 }
